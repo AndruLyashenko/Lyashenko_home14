1526637deca4e940fcc88120d6b453c8fb44211e
AndruLyashenko <laavllag85@gmail.com>
2017-01-25 01:48:57 +0400

    'startfix'

--- source-js/main.js
+++ source-js/main.js
@@ -39,3 +39,24 @@ $('.owl-carousel2').owlCarousel({
         }
     }
 })
+
+$('.owl-carousel3').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
